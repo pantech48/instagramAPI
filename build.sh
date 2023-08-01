2caa644cf45fe0a8b0a20af30e652d966f149598
Yevgeniy Putrin <yevgeniy.putrin@dxc.com>
2023-08-01 02:02:25 +0200

    replacing docker file to "docker dir"

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Step 1: Stop all running containers (optional)
+# Step 1: Stop all running containers
 docker stop $(docker ps -aq)
 
 # Step 2: Remove old image
 docker rmi -f instagramapi-web
 
 # Step 3: Build the Docker image
-docker build -t instagramapi-web .
+docker build -t instagramapi-web -f docker/Dockerfile .
 
 
